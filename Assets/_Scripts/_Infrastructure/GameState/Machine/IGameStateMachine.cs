1ae98304ca2ced984a10c35e629b0f8476b5663b
Anton Kazakevich <71589375+pipistero@users.noreply.github.com>
2024-04-12 05:37:02 +0300

    Update IGameStateMachine.cs

--- Assets/_Scripts/_Infrastructure/GameState/Machine/IGameStateMachine.cs
+++ Assets/_Scripts/_Infrastructure/GameState/Machine/IGameStateMachine.cs
@@ -2,6 +2,8 @@ namespace Infrastructure.GameState.Machine
 {
     public interface IGameStateMachine
     {
+        IExitableState ActiveState { get; }
+        
         void Enter<TState>() where TState : class, IState;
         void Enter<TState, TPayload>(TPayload payload) where TState : class, IPayloadedState<TPayload>;
     }
