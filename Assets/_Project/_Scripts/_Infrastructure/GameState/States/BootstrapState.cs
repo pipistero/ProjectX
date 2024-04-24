e09e2a69660a3ae21f3612029206706055b390b6
Anton Kazakevich <71589375+pipistero@users.noreply.github.com>
2024-04-25 02:40:40 +0300

    Update BootstrapState.cs

--- Assets/_Project/_Scripts/_Infrastructure/GameState/States/BootstrapState.cs
+++ Assets/_Project/_Scripts/_Infrastructure/GameState/States/BootstrapState.cs
@@ -1,20 +1,17 @@
-using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Infrastructure.GameState.Machine;
 using Localization.Service;
 using Widgets.Controller;
-using Zenject;
 
 namespace Infrastructure.GameState.States
 {
     public class BootstrapState : IState
     {
-        private IGameStateMachine _gameStateMachine;
-        private IWidgetsController _widgetsController;
-        private ILocalizationService _localizationService;
+        private readonly IGameStateMachine _gameStateMachine;
+        private readonly IWidgetsController _widgetsController;
+        private readonly ILocalizationService _localizationService;
 
-        [Inject]
-        private void Construct(IGameStateMachine gameStateMachine, IWidgetsController widgetsController, ILocalizationService localizationService)
+        public BootstrapState(IGameStateMachine gameStateMachine, IWidgetsController widgetsController, ILocalizationService localizationService)
         {
             _gameStateMachine = gameStateMachine;
             _widgetsController = widgetsController;
