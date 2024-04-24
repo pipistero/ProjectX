2a2527584633faa8a0e7129a1e9866bbff1acb48
Anton Kazakevich <71589375+pipistero@users.noreply.github.com>
2024-04-25 02:43:57 +0300

    Update MenuState.cs

--- Assets/_Project/_Scripts/_Infrastructure/GameState/States/MenuState.cs
+++ Assets/_Project/_Scripts/_Infrastructure/GameState/States/MenuState.cs
@@ -1,16 +1,14 @@
 using Infrastructure.GameState.Machine;
 using Widgets.Controller;
 using Widgets.Test;
-using Zenject;
 
 namespace Infrastructure.GameState.States
 {
     public class MenuState : IState
     {
-        private IWidgetsController _widgetsController;
+        private readonly IWidgetsController _widgetsController;
 
-        [Inject]
-        private void Construct(IWidgetsController widgetsController)
+        public MenuState(IWidgetsController widgetsController)
         {
             _widgetsController = widgetsController;
         }
